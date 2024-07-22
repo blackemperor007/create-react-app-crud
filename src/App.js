c59dc12ca91052aaf9acb162b47178631c436d24
Cyborg-44 <emperorb007@gmail.com>
2024-07-22 13:14:31 +0100

    mise à jour

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/ReactToastify.css";
+
+// importation des fichiers
+import Home from "./pages/Home";
+import About from "./pages/About";
+import AddEdit from "./pages/AddEdit";
+import View from "./pages/View";
+
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-     <h2>Applicarion React CRUD</h2>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header/>
+        <ToastContainer />
+        <Routes>
+          <Route exact path="/" Component={Home} />
+          <Route path="/add" Component={AddEdit} />
+          <Route path="/update/:id" Component={AddEdit} />
+          <Route path="/view/:id" Component={View} />
+          <Route path="/about" Component={About} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
